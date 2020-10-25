a993c12640458d82f5d3f1c6820460199b882ab1
AlwaysHungrie <dhairyashah98@gmail.com>
2020-10-25 07:46:47 +0530

    touch events

--- public/script.js
+++ public/script.js
@@ -3,6 +3,27 @@ const icon = document.querySelector('.water-icon');
 const shadow = document.querySelector('.water-shadow');
 const contents = document.querySelector('.water-container-contents');
 
+waterButton.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation(); // not necessary in my case, could leave in case stopImmediateProp isn't available? 
+  event.stopImmediatePropagation();
+  return false;
+};
+
+icon.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation(); // not necessary in my case, could leave in case stopImmediateProp isn't available? 
+  event.stopImmediatePropagation();
+  return false;
+};
+
+shadow.oncontextmenu = function(event) {
+  event.preventDefault();
+  event.stopPropagation(); // not necessary in my case, could leave in case stopImmediateProp isn't available? 
+  event.stopImmediatePropagation();
+  return false;
+};
+
 fetch('/waterLevel', {
   method: 'GET',
 }).then(response => response.json()).then(data => {
@@ -52,6 +73,43 @@ fetch('/waterLevel', {
     waterButton.addEventListener('mouseup', stopFlow);
   });
 
+  waterButton.addEventListener('touchstart', () => {
+    if (hardStop) return;
+    
+    console.log('flow activated')
+    fetch('/start', {
+      method: 'POST',
+      body: JSON.stringify({
+        'command': 'start'
+      })
+    });
+
+    buttonActive = true;
+    icon.classList.add('water-icon-active');
+    shadow.classList.add('water-shadow-active');
+
+    emitInterval = setInterval(() => {
+      waterLevel += 0.5;
+      if (waterLevel >= 100) {
+        stopFlow();
+        waterButton.removeEventListener('touchend', stopFlow);
+        hardStop = true;
+      }
+      contents.style.transform = `translateY(${waterLevel}%)`;
+
+      fetch('/status', {
+        method: 'GET',
+      }).then(response => response.json()).then(data => {
+        if (data.status == false && buttonActive == true) {
+          stopFlow();
+          waterButton.removeEventListener('touchend', stopFlow);
+        }
+      });
+    }, 100);
+
+    waterButton.addEventListener('touchend', stopFlow);
+  });
+
   function stopFlow () {
     console.log('flow stopped')
     fetch('/stop', {
@@ -74,5 +132,12 @@ fetch('/waterLevel', {
       stopFlow();
     }
     waterButton.removeEventListener('mouseup', stopFlow);
+  });
+
+  waterButton.addEventListener('touchcancel', () => {
+    if (buttonActive) {
+      stopFlow();
+    }
+    waterButton.removeEventListener('touchend', stopFlow);
   });
 });
